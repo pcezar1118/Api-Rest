2d9fab645a506be60725d26268d97d23d53e7444
pcezar1118 <pcezar1118@gmail.com>
2023-01-19 17:50:06 -0300

    mysql

---
+++ infra/conexao.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql'
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'Pcezar11@*',
+    database: 'bd-copa'
+})
+
+
+export default conexao
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import app from './src/app.js'
+
+import conexao from './infra/conexao.js'
+
 const PORT = 3000
 
+//fazer a conexao
+conexao.connect((erro) => {
+    if(erro) {
+        console.log(erro)
+    }else {console.log('Conexão realizada com sucesso!')}
 //Escutar a porta 3000
 app.listen(PORT, () => {
     console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
 })
+
+})
+
